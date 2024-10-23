6f309dfbf73059ca9ab972a37b5ef2ee244fa0d2
ClaireYarber <clyarber@ou.edu>
2024-10-23 13:15:46 -0500

    Update page4.html

--- page4.php
+++ page4.php
@@ -1,4 +1,15 @@
-<h2>What Can JavaScript Do?</h2>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <h1>MIS 4013 Homework 5</h1>
+    <a href="page5.html">Page 5</a>
+    <title>Homework 5</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+</html><h2>What Can JavaScript Do?</h2>
 
 <p>JavaScript can change HTML attribute values.</p>
 
@@ -9,3 +20,6 @@
 <img id="myImage" src="pic_bulboff.gif" style="width:100px">
 
 <button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
