a515274d5a6bf4db7dfffadfa86ddc73c7d432ca
ClaireYarber <clyarber@ou.edu>
2024-10-23 13:16:12 -0500

    Update page4.html

--- page4.php
+++ page4.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-</html><h2>What Can JavaScript Do?</h2>
+<h2>What Can JavaScript Do?</h2>
 
 <p>JavaScript can change HTML attribute values.</p>
 
